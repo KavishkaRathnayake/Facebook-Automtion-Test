5b39b04a9e609765bbe84a423e8fdd8f195d8d8e
KavishkaRathnayake <medhangakavishka06@gmail.com>
2024-09-29 19:45:54 +0530

    First commit

---
+++ Facebook-Automation-Test/src/test/java/HomePageTest.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomePageTest {
+    WebDriver driver;
+
+    public void OpenHomePage(){
+        driver = new ChromeDriver();
+
+
+    }
+}
+
