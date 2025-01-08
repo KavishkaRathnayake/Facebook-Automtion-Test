5578e7d76ad8750f0b8fd772be11a5acddd6a615
Kavishka <medhangakavishka06@gmail.com>
2025-01-09 05:08:16 +0530

    1.8.3 - Verify Remove Friend Request

--- Facebook-Automation-Test/src/test/java/Friends/TC_FB_Friends_3.java
+++ Facebook-Automation-Test/src/test/java/Friends/TC_FB_Friends_3.java
@@ -13,15 +13,20 @@ import java.time.Duration;
 public class TC_FB_Friends_3 {
     WebDriver driver;
 
-    @Test(priority = 1)
-    public void TC_FB_Friends_3Test() {
+    public void TC_FB_Friends_3(){
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.setBrowserVersion("121");
         driver = new ChromeDriver(chromeOptions);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         driver.manage().window().maximize();
         driver.get("https://www.facebook.com/");
+    }
 
+    @Test(priority = 1)
+    public void TC_FB_Friends_3Test() {
+
+        //User A
+        //Login to facebook
         WebElement username = driver.findElement(By.id("email"));
         username.sendKeys("patali9600@gmail.com");
 
@@ -48,13 +53,9 @@ public class TC_FB_Friends_3 {
 
     @Test(priority = 2)
     public void TC_FB_Friends_3Test1() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setBrowserVersion("121");
-        driver = new ChromeDriver(chromeOptions);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-        driver.manage().window().maximize();
-        driver.get("https://www.facebook.com/");
 
+        //User B
+        //Login to facebook
         WebElement username = driver.findElement(By.id("email"));
         username.sendKeys("jegoh94617@konican.com");
 
@@ -64,6 +65,7 @@ public class TC_FB_Friends_3 {
         WebElement LoginButton = driver.findElement(By.xpath("(//button[normalize-space()='Log in'])[1]"));
         LoginButton.click();
 
+        //Delete request
         WebElement FriendSection = driver.findElement(By.xpath("//span[contains(text(),'Find friends')]"));
         FriendSection.click();
         WebElement FriendRequests = driver.findElement(By.xpath("//span[@class='x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lkfr7t x1lbecb7 xk50ysn xzsf02u'][normalize-space()='Friend Requests']"));
@@ -71,8 +73,7 @@ public class TC_FB_Friends_3 {
         WebElement Delete = driver.findElement(By.xpath("//div[@class='x135pmgq']//div//div//span[@class='x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x3x7a5m x6prxxf xvq8zen x1s688f x1dem4cn']//span[@class='x1lliihq x6ikm8r x10wlt62 x1n2onr6 xlyipyv xuxw1ft'][normalize-space()='Delete']"));
         Delete.click();
 
-
-
+        //Verify
         WebElement verify = driver.findElement(By.xpath("//div[@class='x1huwwth x16suv20']"));
         String Request = verify.getText();
         System.out.println("Button Shows: " + Request);
