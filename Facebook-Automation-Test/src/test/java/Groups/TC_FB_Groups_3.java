5c78c6e8660dbe8ca19b0cf4bc6944273e981e3c
Kavishka <medhangakavishka06@gmail.com>
2025-01-13 07:52:12 +0530

    1.10.3 - Verify that the user can create a post in a group.

---
+++ Facebook-Automation-Test/src/test/java/Groups/TC_FB_Groups_3.java
@@ -0,0 +1,84 @@
+package Groups;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.time.Duration;
+
+public class TC_FB_Groups_3 {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void TC_FB_Groups_3(){
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setBrowserVersion("121");
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.manage().window().maximize();
+        driver.get("https://www.facebook.com/");
+    }
+
+    @Test
+    public void TC_FB_Groups_3Test() throws AWTException, InterruptedException {
+        //Login to facebook
+        WebElement username = driver.findElement(By.id("email"));
+        username.sendKeys("jegoh94617@konican.com");
+
+        WebElement Password = driver.findElement(By.id("pass"));
+        Password.sendKeys("celkon");
+
+        WebElement LoginButton = driver.findElement(By.xpath("(//button[normalize-space()='Log in'])[1]"));
+        LoginButton.click();
+
+        //Search
+        WebElement search = driver.findElement(By.xpath("//input[@placeholder='Search Facebook']"));
+        search.sendKeys("Aliexpress coupon ඉක්මන්ට ගනිමු" + Keys.ENTER);
+        //Visit Group
+        WebElement Group = driver.findElement(By.xpath("//a[contains(text(),'Aliexpress coupon ඉක්මන්ට ගනිමු')]"));
+        Group.click();
+
+        //Create post
+        WebElement TextBox = driver.findElement(By.xpath("//span[normalize-space()='Write something...']"));
+        TextBox.click();
+
+        //image
+        WebElement Image = driver.findElement(By.xpath("//div[@aria-label='Photo/video']//div//div//img[@class='x1b0d499 xl1xv1r']"));
+        Image.click();
+        String Data = "C:\\Users\\Kavishka\\Downloads\\FB post.png";
+        StringSelection selection = new StringSelection(Data);
+
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection , null);
+
+        Robot robot= new Robot();
+        robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_CONTROL);
+        Thread.sleep(1500);
+
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+
+        //Text
+        WebElement Text = driver.findElement(By.xpath("//div[@class='x1ejq31n xd10rxx x1sy0etr x17r0tee x9f619 xzsf02u xmper1u xngnso2 xo1l8bm x5yr21d x1qb5hxa x1a2a7pz x1iorvi4 x4uap5 xwib8y2 xkhd6sd xh8yej3 xha3pab']"));
+        Text.sendKeys("This is a Test Post by -KAVISHKA-");
+
+        //post
+        WebElement post = driver.findElement(By.xpath("//div[@aria-label='Post']//div[@class='x6s0dn4 x78zum5 xl56j7k x1608yet xljgi0e x1e0frkt']"));
+        post.click();
+
+        Thread.sleep(2000);
+        System.out.println("Yes, user successfully created a post on group");
+
+
+    }
+}
