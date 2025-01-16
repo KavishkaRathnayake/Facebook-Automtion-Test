56935ba8e44291e61e223b593ffc5b1902dbdcaa
Kavishka <medhangakavishka06@gmail.com>
2025-01-16 07:35:41 +0530

    1.12.2 - Verify Navigation to Reels Section

---
+++ Facebook-Automation-Test/src/test/java/Reels/TC_FB_Reels_2.java
@@ -0,0 +1,55 @@
+package Reels;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class TC_FB_Reels_2 {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void TC_FB_Reels_2() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setBrowserVersion("121");
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.manage().window().maximize();
+        driver.get("https://www.facebook.com/");
+    }
+
+    @Test
+    public void TC_FB_Reels_2Test() throws InterruptedException {
+        //Login to facebook
+        WebElement username = driver.findElement(By.id("email"));
+        username.sendKeys("jegoh94617@konican.com");
+
+        WebElement Password = driver.findElement(By.id("pass"));
+        Password.sendKeys("celkon");
+
+        WebElement LoginButton = driver.findElement(By.xpath("(//button[normalize-space()='Log in'])[1]"));
+        LoginButton.click();
+
+        //Visit to videos
+        WebElement Videos = driver.findElement(By.xpath("//a[@aria-label='Video']"));
+        Videos.click();
+
+        //Visit to reels
+        WebElement Reels = driver.findElement(By.xpath("//a[@href='/reel/?s=bookmark']//div[@class='x6s0dn4 x1q0q8m5 x1qhh985 xu3j5b3 xcfux6l x26u7qi xm0m39n x13fuv20 x972fbf x9f619 x78zum5 x1q0g3np x1iyjqo2 xs83m0k x1qughib xat24cr x11i5rnm x1mh8g0r xdj266r xeuugli x18d9i69 x1sxyh0 xurb0ha xexx8yu x1n2onr6 x1ja2u2z x1gg8mnh']"));
+        Reels.click();
+
+
+        Thread.sleep(2000);
+        String Verify =  driver.getTitle();
+        System.out.println("User visited to: " + Verify);
+
+
+        driver.quit();
+
+    }
+}
