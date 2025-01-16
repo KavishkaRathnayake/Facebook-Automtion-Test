43ff1b20e18884d25112220a964f0b8730102b49
Kavishka <medhangakavishka06@gmail.com>
2025-01-16 07:35:12 +0530

    1.12.1 - Verify Navigation to Videos Section

---
+++ Facebook-Automation-Test/src/test/java/Reels/TC_FB_Reels_1.java
@@ -0,0 +1,50 @@
+package Reels;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class TC_FB_Reels_1 {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void TC_FB_Reels_1() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setBrowserVersion("121");
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.manage().window().maximize();
+        driver.get("https://www.facebook.com/");
+    }
+
+    @Test
+    public void TC_FB_Reels_1Test() throws InterruptedException {
+        //Login to facebook
+        WebElement username = driver.findElement(By.id("email"));
+        username.sendKeys("jegoh94617@konican.com");
+
+        WebElement Password = driver.findElement(By.id("pass"));
+        Password.sendKeys("celkon");
+
+        WebElement LoginButton = driver.findElement(By.xpath("(//button[normalize-space()='Log in'])[1]"));
+        LoginButton.click();
+
+        //Visit to videos
+        WebElement visit = driver.findElement(By.xpath("//a[@aria-label='Video']"));
+        visit.click();
+
+        Thread.sleep(2000);
+        String Verify =  driver.getTitle();
+        System.out.println("User visited to: " + Verify);
+
+
+        driver.quit();
+
+    }
+}
