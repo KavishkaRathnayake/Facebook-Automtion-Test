ac5e5c00eba212b5d040d1af298c14eea523fb54
Kavishka Rathnayake <medhangakavishka06@gmail.com>
2025-01-27 06:46:18 +0530

    1.0.5 - Verify Login Functionality by Dosesn't entering a username and password

---
+++ Facebook-Automation-Test/src/test/java/LogIn/TC_FB_Login_05.java
@@ -0,0 +1,44 @@
+package LogIn;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class TC_FB_Login_05 {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void openLoginTest() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setBrowserVersion("121");
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.get("https://www.facebook.com/");
+    }
+    @Test
+    public void TC_FB_Login_05(){
+        //     Don't Enter a username & password
+        WebElement LoginButton = driver.findElement(By.xpath("(//button[normalize-space()='Log in'])[1]"));
+        LoginButton.click();
+
+        invalidCredentials2();
+
+        driver.quit();
+
+    }
+    //Error message when entering wrong credentials. (But that error message is Showing sometimes only )
+    public void invalidCredentials2() {
+        WebElement invalidCredentials2 = driver.findElement(By.xpath("//div[@class='_9ay7']"));
+        String errormessage = invalidCredentials2.getText();
+        System.out.println("Showing an error as: " + errormessage);
+
+    }
+
+}
