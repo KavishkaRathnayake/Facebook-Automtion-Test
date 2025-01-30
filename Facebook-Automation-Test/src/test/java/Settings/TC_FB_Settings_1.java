c5da71e2694f1fc113a0e1995d8a7b523bdd5983
Kavishka Rathnayake <medhangakavishka06@gmail.com>
2025-01-30 08:08:20 +0530

    1.13.1 - Verify that the user can access the settings page.

--- Facebook-Automation-Test/src/test/java/Settings/TC_FB_Settings_1.java
+++ Facebook-Automation-Test/src/test/java/Settings/TC_FB_Settings_1.java
@@ -40,7 +40,7 @@ public class TC_FB_Settings_1 {
         ProfileIcon.click();
         WebElement PrivacyAndSettings = driver.findElement(By.xpath("//span[normalize-space()='Settings & privacy']"));
         PrivacyAndSettings.click();
-        WebElement Settings = driver.findElement(By.xpath("//a[@href='/settings']//div[@class='x6s0dn4 x1q0q8m5 x1qhh985 xu3j5b3 xcfux6l x26u7qi xm0m39n x13fuv20 x972fbf x9f619 x78zum5 x1q0g3np x1iyjqo2 xs83m0k x1qughib xat24cr x11i5rnm x1mh8g0r xdj266r xeuugli x18d9i69 x1sxyh0 xurb0ha xexx8yu x1n2onr6 x1ja2u2z x1gg8mnh']//div[@class='x78zum5 xdt5ytf xq8finb x1n2onr6 xqcrz7y x1k70j0n xzueoph']//div[@class='html-div xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x14yjl9h xudhj91 x18nykt9 xww2gxu x6s0dn4 x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x3nfvp2 xl56j7k x1n2onr6 x1qhmfi1 xc9qbxq x14qfxbe']//i[@class='x1b0d499 xep6ejk']"));
+        WebElement Settings = driver.findElement(By.xpath("//a[@href='/settings']//div[@class='x6s0dn4 x1q0q8m5 x1qhh985 xu3j5b3 xcfux6l x26u7qi xm0m39n x13fuv20 x972fbf x9f619 x78zum5 x1q0g3np x1iyjqo2 xs83m0k x1qughib xat24cr x11i5rnm x1mh8g0r xdj266r xeuugli x18d9i69 x1sxyh0 xurb0ha xexx8yu x1n2onr6 x1ja2u2z x1gg8mnh']//div[@class='x6s0dn4 xkh2ocl x1q0q8m5 x1qhh985 xu3j5b3 xcfux6l x26u7qi xm0m39n x13fuv20 x972fbf x9f619 x78zum5 x1q0g3np x1iyjqo2 xs83m0k x1qughib xat24cr x11i5rnm x1mh8g0r xdj266r x2lwn1j xeuugli x18d9i69 x4uap5 xkhd6sd xexx8yu x1n2onr6 x1ja2u2z']"));
         Settings.click();
 
         driver.navigate().back();
