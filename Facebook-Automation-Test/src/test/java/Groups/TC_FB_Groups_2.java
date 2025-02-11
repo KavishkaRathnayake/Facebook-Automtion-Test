50e7ddd8ecf9ef1abf245311b14c959ee768c457
Kavishka Rathnayake <medhangakavishka06@gmail.com>
2025-02-11 11:19:02 +0530

    1.10.2 - Verify that the user can join a group successfully.

--- Facebook-Automation-Test/src/test/java/Groups/TC_FB_Groups_2.java
+++ Facebook-Automation-Test/src/test/java/Groups/TC_FB_Groups_2.java
@@ -7,7 +7,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.decorators.WebDriverDecorator;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -26,13 +28,14 @@ public class TC_FB_Groups_2 {
     }
 
     @Test
-    public void TC_FB_Groups_2Test() throws InterruptedException {
+    @Parameters({"Username","Password"})
+    public void TC_FB_Groups_2Test(String Username,String Pass) throws InterruptedException {
         //Login to facebook
         WebElement username = driver.findElement(By.id("email"));
-        username.sendKeys("jegoh94617@konican.com");
+        username.sendKeys(Username);
 
         WebElement Password = driver.findElement(By.id("pass"));
-        Password.sendKeys("celkon");
+        Password.sendKeys(Pass);
 
         WebElement LoginButton = driver.findElement(By.xpath("(//button[normalize-space()='Log in'])[1]"));
         LoginButton.click();
@@ -44,7 +47,7 @@ public class TC_FB_Groups_2 {
         WebElement Group = driver.findElement(By.xpath("//a[contains(text(),'Aliexpress coupon ඉක්මන්ට ගනිමු')]"));
         Group.click();
         //Join Group
-        WebElement join = driver.findElement(By.xpath("//div[@class='x9f619 x1n2onr6 x1ja2u2z x78zum5 xdt5ytf x2lah0s x193iq5w xeuugli x150jy0e x1e558r4 xjkvuk6 x1iorvi4']//span[@class='x1lliihq x6ikm8r x10wlt62 x1n2onr6 xlyipyv xuxw1ft'][normalize-space()='Join Group']"));
+        WebElement join = driver.findElement(By.xpath("(//div[@role='none'])[91]//span[text()='Join Group']"));
         String joinButton = join.getText();
         System.out.println("The button text before join the group: " + joinButton);
         join.click();
@@ -52,13 +55,19 @@ public class TC_FB_Groups_2 {
         Thread.sleep(3000);
 
         //Verify joined button
-        WebElement joined = driver.findElement(By.xpath("//div[@class='x9f619 x1n2onr6 x1ja2u2z x78zum5 xdt5ytf x2lah0s x193iq5w xeuugli x150jy0e x1e558r4 xjkvuk6 x1iorvi4']//span[@class='x1lliihq x6ikm8r x10wlt62 x1n2onr6 xlyipyv xuxw1ft'][normalize-space()='Joined']"));
+        WebElement joined = driver.findElement(By.xpath("//span[contains(text(),'Joined')]"));
         String isjoined = joined.getText();
         System.out.println("After Joined to the group the join button changed as: " + isjoined);
 
         Thread.sleep(2000);
         System.out.println("Yes, The 'Join Group' button changes to reflect the new membership status");
+
         driver.quit();
     }
+    @AfterMethod
+         public void Quiet(){
+                driver.quit();
+}
+
 
 }
