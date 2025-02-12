1121d4ea2268424c9b8080d9f2cf9f7d252dfea2
Kavishka Rathnayake <medhangakavishka06@gmail.com>
2025-02-13 00:29:46 +0530

    1.0.1 - Verify Login Functionality by Entering a valid username & valid password

--- Facebook-Automation-Test/src/test/java/LogIn/TC_FB_Login_01.java
+++ Facebook-Automation-Test/src/test/java/LogIn/TC_FB_Login_01.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -19,30 +21,34 @@ public class TC_FB_Login_01 {
         chromeOptions.setBrowserVersion("121");
         driver = new ChromeDriver(chromeOptions);
         driver.manage().window().maximize();
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         driver.get("https://www.facebook.com/");
     }
 
     @Test
-    public void TC_FB_Login_01() {
+    @Parameters({"Username","Password"})
+    public void TC_FB_Login_01(String user, String pass) {
 //      Enter a valid username & valid password
 
         WebElement username = driver.findElement(By.id("email"));
-        username.sendKeys("jigiwi7156@jofuso.com");
+        username.sendKeys(user);
 
         WebElement Password = driver.findElement(By.id("pass"));
-        Password.sendKeys("Celkon");
+        Password.sendKeys(pass);
 
         WebElement LoginButton = driver.findElement(By.xpath("(//button[normalize-space()='Log in'])[1]"));
         LoginButton.click();
 
-        //Sometimes while Anonymous Login it is requiring the password again, for that;
-        WebElement AnonymousLoginPassword = driver.findElement(By.id("pass"));
-        AnonymousLoginPassword.sendKeys("Celkon");
+//        //Sometimes while Anonymous Login it is requiring the password again, for that;
+//        WebElement AnonymousLoginPassword = driver.findElement(By.id("pass"));
+//        AnonymousLoginPassword.sendKeys("Celkon");
+//
+//        WebElement AnonymousLoginButton = driver.findElement(By.xpath("(//button[normalize-space()='Log in'])[1]"));
+//        AnonymousLoginButton.click();
 
-        WebElement AnonymousLoginButton = driver.findElement(By.xpath("(//button[normalize-space()='Log in'])[1]"));
-        AnonymousLoginButton.click();
-
-
+    }
+    @AfterMethod
+    public void after(){
+        driver.quit();
     }
 }
