98a0434b414fd1625fa17b97b0f9515c77b87b52
Kavishka Rathnayake <medhangakavishka06@gmail.com>
2025-02-14 00:05:39 +0530

    1.8.4 - Verify Unfollow Functionalityrify Remove Friend Request

--- Facebook-Automation-Test/src/test/java/Friends/TC_FB_Friends_4.java
+++ Facebook-Automation-Test/src/test/java/Friends/TC_FB_Friends_4.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -18,19 +20,20 @@ public class TC_FB_Friends_4 {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.setBrowserVersion("121");
         driver = new ChromeDriver(chromeOptions);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1000));
         driver.manage().window().maximize();
         driver.get("https://www.facebook.com/");
     }
 
     @Test
-    public void TC_FB_Friends_5Test() throws InterruptedException {
+    @Parameters({"Username","Password"})
+    public void TC_FB_Friends_5Test(String user, String Pass) throws InterruptedException {
         //Login to facebook
         WebElement username = driver.findElement(By.id("email"));
-        username.sendKeys("patali9600@gmail.com");
+        username.sendKeys(user);
 
         WebElement Password = driver.findElement(By.id("pass"));
-        Password.sendKeys("celkon");
+        Password.sendKeys(Pass);
 
         WebElement LoginButton = driver.findElement(By.xpath("(//button[normalize-space()='Log in'])[1]"));
         LoginButton.click();
@@ -54,6 +57,10 @@ public class TC_FB_Friends_4 {
         WebElement Verify = driver.findElement(By.xpath("//span[normalize-space()='Follow Kavishka']"));
         String Follow = Verify.getText();
         System.out.println("Button Text Changes as: " + Follow);
+
+    }
+    @AfterMethod
+    public void quit(){
         driver.quit();
     }
 }
