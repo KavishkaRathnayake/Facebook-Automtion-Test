b7df122e86bf03f380fa00f7bf3d102fd5a4863e
Kavishka Rathnayake <medhangakavishka06@gmail.com>
2025-02-14 00:04:35 +0530

    1.8.1 - Verify Send Friend Request

--- Facebook-Automation-Test/src/test/java/Friends/TC_FB_Friends_1.java
+++ Facebook-Automation-Test/src/test/java/Friends/TC_FB_Friends_1.java
@@ -6,7 +6,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -14,14 +16,8 @@ import java.time.Duration;
 public class TC_FB_Friends_1 {
     WebDriver driver;
 
-////    @BeforeMethod
-////    public void TC_FB_Friends_1(){
-//
-//
-//    }
-
-    @Test(priority = 1)
-    public void TC_FB_Friends_1Test() {
+    @BeforeMethod
+    public void TC_FB_Friends_1(){
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.setBrowserVersion("121");
         driver = new ChromeDriver(chromeOptions);
@@ -29,31 +25,41 @@ public class TC_FB_Friends_1 {
         driver.manage().window().maximize();
         driver.get("https://www.facebook.com/");
 
+}
+
+    @Test
+    @Parameters({"Username", "Password"})
+    public void TC_FB_Friends_1Test(String Username, String pass) throws InterruptedException {
+
         WebElement username = driver.findElement(By.id("email"));
-        username.sendKeys("patali9600@gmail.com");
+        username.sendKeys(Username);
 
         WebElement Password = driver.findElement(By.id("pass"));
-        Password.sendKeys("celkon");
+        Password.sendKeys(pass);
 
         WebElement LoginButton = driver.findElement(By.xpath("(//button[normalize-space()='Log in'])[1]"));
         LoginButton.click();
 
         WebElement Search = driver.findElement(By.xpath("//input[@placeholder='Search Facebook']"));
-        Search.sendKeys("Kavishka Selenium" + Keys.ENTER);
+        Search.sendKeys("rahul jekhar" + Keys.ENTER);
 
-        WebElement GoToProfile = driver.findElement(By.xpath("//a[normalize-space()='Kavishka Selenium']"));
+        WebElement GoToProfile = driver.findElement(By.xpath("//a[@aria-label='Rahul Jekhar']//span[contains(text(),'Rahul Jekhar')]"));
         GoToProfile.click();
+        Thread.sleep(1200);
 
         WebElement AddFriendButton = driver.findElement(By.xpath("//span[contains(text(),'Add friend')]"));
         AddFriendButton.click();
 
-        WebElement AddFriendButtonConfirmation = driver.findElement(By.xpath("//body/div/div/div/div/div/div/div/div/div/div/div/div/div[@role='dialog']/div/div/div[@aria-label='Add friend']/div[1]"));
+        WebElement AddFriendButtonConfirmation = driver.findElement(By.xpath("//span[contains(text(),'Cancel request')]"));
         AddFriendButtonConfirmation.click();
 
         WebElement verify = driver.findElement(By.xpath("//span[contains(text(),'Cancel request')]"));
         String Request = verify.getText();
         System.out.println("Button Text Changes as: " + Request);
 
+    }
+    @AfterMethod
+    public void quit(){
         driver.quit();
     }
 
