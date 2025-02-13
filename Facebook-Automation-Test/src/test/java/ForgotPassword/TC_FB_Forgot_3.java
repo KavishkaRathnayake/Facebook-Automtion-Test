6770279189c1137724f0387a0ee9cf0a34a70b9e
Kavishka Rathnayake <medhangakavishka06@gmail.com>
2025-02-13 11:27:59 +0530

    1.4.3 - Resetting Password by giving a valid Mobile Number

--- Facebook-Automation-Test/src/test/java/ForgotPassword/TC_FB_Forgot_3.java
+++ Facebook-Automation-Test/src/test/java/ForgotPassword/TC_FB_Forgot_3.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -24,30 +26,24 @@ public class TC_FB_Forgot_3 {
     }
 
     @Test
-    public void TC_FB_Forgot_1Test(){
+    @Parameters("Username")
+    public void TC_FB_Forgot_3Test(String user){
         //visit to the password reset page
         driver.findElement(By.xpath("(//a[normalize-space()='Forgotten password?'])[1]")).click();
 
         //Enter a Valid Email
-        WebElement EnterEmail = driver.findElement(By.name("email"));
-        EnterEmail.sendKeys("0773681755");
+        WebElement EnterEmail = driver.findElement(By.xpath("//input[@id='identify_email']"));
+        EnterEmail.sendKeys(user);
 
         WebElement SearchButton = driver.findElement(By.xpath("//button[@id='did_submit']"));
         SearchButton.click();
 
         WebElement ContinueButton = driver.findElement(By.xpath("//button[normalize-space()='Continue']"));
         ContinueButton.click();
-//
-//        Boolean EmailButton = driver.findElement(By.name("send_email")).isSelected();
-//        WebElement  PasswordButton = driver.findElement(By.name("recover_method"));
-//        PasswordButton.isSelected();
-//
-//        if((EmailButton)){
-//            WebElement selectEmailButton = driver.findElement(By.name("send_email"));
-//            selectEmailButton.click();
-//        }
 
-
-
+    }
+    @AfterMethod
+    public void quit(){
+        driver.quit();
     }
 }
