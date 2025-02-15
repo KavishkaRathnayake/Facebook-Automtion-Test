e9163b4583b03a41e29b0ccc28bb4ec78d8d93a6
Kavishka Rathnayake <medhangakavishka06@gmail.com>
2025-02-15 09:35:19 +0530

    1.13.5 - Verify that the user can access Activity Log

--- Facebook-Automation-Test/src/test/java/Settings/TC_FB_Settings_5.java
+++ Facebook-Automation-Test/src/test/java/Settings/TC_FB_Settings_5.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -24,13 +26,14 @@ public class TC_FB_Settings_5 {
     }
 
     @Test
-    public void TC_FB_Settings_5Test(){
-        //Login to facebook
+    @Parameters({"Username","Password"})
+    public void TC_FB_Settings_5Test(String user, String Pass) throws InterruptedException {
         WebElement username = driver.findElement(By.id("email"));
-        username.sendKeys("jegoh94617@konican.com");
+        username.sendKeys(user);
 
         WebElement Password = driver.findElement(By.id("pass"));
-        Password.sendKeys("celkon");
+        Password.sendKeys(Pass);
+
 
         WebElement LoginButton = driver.findElement(By.xpath("(//button[normalize-space()='Log in'])[1]"));
         LoginButton.click();
@@ -43,6 +46,10 @@ public class TC_FB_Settings_5 {
         WebElement ActivityLog = driver.findElement(By.xpath("//body[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[5]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[5]/div[1]/div[1]/div[1]/a[1]/div[1]/div[2]"));
         ActivityLog.click();
 
+
+    }
+    @AfterMethod
+    public void after(){
         driver.quit();
     }
 }
