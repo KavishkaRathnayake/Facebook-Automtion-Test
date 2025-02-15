637c479734f5f6bca88a5d2fa26c3a83fd410393
Kavishka Rathnayake <medhangakavishka06@gmail.com>
2025-02-15 09:34:37 +0530

    1.13.4 - Verify that the user can access the privacy centre

--- Facebook-Automation-Test/src/test/java/Settings/TC_FB_Settings_4.java
+++ Facebook-Automation-Test/src/test/java/Settings/TC_FB_Settings_4.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -24,13 +26,14 @@ public class TC_FB_Settings_4 {
     }
 
     @Test
-    public void TC_FB_Settings_4Test(){
-        //Login to facebook
+    @Parameters({"Username","Password"})
+    public void TC_FB_Settings_4Test(String user, String Pass) throws InterruptedException {
         WebElement username = driver.findElement(By.id("email"));
-        username.sendKeys("jegoh94617@konican.com");
+        username.sendKeys(user);
 
         WebElement Password = driver.findElement(By.id("pass"));
-        Password.sendKeys("celkon");
+        Password.sendKeys(Pass);
+
 
         WebElement LoginButton = driver.findElement(By.xpath("(//button[normalize-space()='Log in'])[1]"));
         LoginButton.click();
@@ -43,7 +46,10 @@ public class TC_FB_Settings_4 {
         WebElement PrivacyCentre = driver.findElement(By.xpath("//a[@href='/privacy/center/?entry_point=facebook_bookmarks']//div[@class='x6s0dn4 x1q0q8m5 x1qhh985 xu3j5b3 xcfux6l x26u7qi xm0m39n x13fuv20 x972fbf x9f619 x78zum5 x1q0g3np x1iyjqo2 xs83m0k x1qughib xat24cr x11i5rnm x1mh8g0r xdj266r xeuugli x18d9i69 x1sxyh0 xurb0ha xexx8yu x1n2onr6 x1ja2u2z x1gg8mnh']//div[@class='x6s0dn4 xkh2ocl x1q0q8m5 x1qhh985 xu3j5b3 xcfux6l x26u7qi xm0m39n x13fuv20 x972fbf x9f619 x78zum5 x1q0g3np x1iyjqo2 xs83m0k x1qughib xat24cr x11i5rnm x1mh8g0r xdj266r x2lwn1j xeuugli x18d9i69 x4uap5 xkhd6sd xexx8yu x1n2onr6 x1ja2u2z']"));
         PrivacyCentre.click();
 
-        driver.quit();
 
+    }
+    @AfterMethod
+    public void after(){
+        driver.quit();
     }
 }
