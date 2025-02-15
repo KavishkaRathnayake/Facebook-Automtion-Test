b68980ca4833956f9cce787d2dee2955e4a9a6f5
Kavishka Rathnayake <medhangakavishka06@gmail.com>
2025-02-15 09:36:31 +0530

    1.13.8 - Verify that the user can logout from Facebook

--- Facebook-Automation-Test/src/test/java/Settings/TC_FB_Settings_8.java
+++ Facebook-Automation-Test/src/test/java/Settings/TC_FB_Settings_8.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -23,13 +25,14 @@ public class TC_FB_Settings_8 {
     }
 
     @Test
-    public void TC_FB_Settings_8Test() throws InterruptedException {
-        //Login to facebook
+    @Parameters({"Username","Password"})
+    public void TC_FB_Settings_8Test(String user, String Pass) throws InterruptedException {
         WebElement username = driver.findElement(By.id("email"));
-        username.sendKeys("jegoh94617@konican.com");
+        username.sendKeys(user);
 
         WebElement Password = driver.findElement(By.id("pass"));
-        Password.sendKeys("celkon");
+        Password.sendKeys(Pass);
+
 
         WebElement LoginButton = driver.findElement(By.xpath("(//button[normalize-space()='Log in'])[1]"));
         LoginButton.click();
@@ -41,7 +44,10 @@ public class TC_FB_Settings_8 {
         WebElement Logout = driver.findElement(By.xpath("//span[normalize-space()='Log out']"));
         Logout.click();
 
-        driver.quit();
 
+    }
+    @AfterMethod
+    public void after(){
+        driver.quit();
     }
 }
