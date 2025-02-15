a58f425d448fc24884cee9314489f4e4ad158864
Kavishka Rathnayake <medhangakavishka06@gmail.com>
2025-02-15 09:36:14 +0530

    1.13.7 - Verify that the user can switch to compact mode

--- Facebook-Automation-Test/src/test/java/Settings/TC_FB_Settings_7.java
+++ Facebook-Automation-Test/src/test/java/Settings/TC_FB_Settings_7.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -24,13 +26,14 @@ public class TC_FB_Settings_7 {
     }
 
     @Test
-    public void TC_FB_Settings_7Test() throws InterruptedException {
-        //Login to facebook
+    @Parameters({"Username","Password"})
+    public void TC_FB_Settings_7Test(String user, String Pass) throws InterruptedException {
         WebElement username = driver.findElement(By.id("email"));
-        username.sendKeys("jegoh94617@konican.com");
+        username.sendKeys(user);
 
         WebElement Password = driver.findElement(By.id("pass"));
-        Password.sendKeys("celkon");
+        Password.sendKeys(Pass);
+
 
         WebElement LoginButton = driver.findElement(By.xpath("(//button[normalize-space()='Log in'])[1]"));
         LoginButton.click();
@@ -44,7 +47,11 @@ public class TC_FB_Settings_7 {
         WebElement compactMode = driver.findElement(By.xpath("//input[@value='on']"));
         compactMode.click();
 
-        driver.quit();
 
+
+    }
+    @AfterMethod
+    public void after(){
+        driver.quit();
     }
 }
